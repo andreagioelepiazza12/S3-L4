d01d336b9ad65bdd0310abde8a5d2aee55a01324
andreagioelepiazza12 <andreagioelepiazza@gmail.com>
2024-10-17 17:24:31 +0200

    first commit

--- js/js.js
+++ js/js.js
@@ -13,6 +13,36 @@
             tabellone.appendChild(cell);
         }
 
+        const tabellina = document.getElementById("tabellina");
+    
+        const input = document.getElementById("tabInput");
+        const tabBtn = document.getElementById("invio");
+
+
+        tabBtn.addEventListener("click", () => {
+
+        for(let y = 0; y<parseInt(input.value); y++){
+
+            for (let i = 1; i <= 24; i++) {
+                let numeriTabellina = [];
+                let num ;
+              
+                do {
+                    num = Math.floor(Math.random() * 76) + 1;
+                } while (numeriTabellina.includes(num));
+                numeriTabellina.push(num);
+
+                const cell = document.createElement("div");
+                cell.classList.add("cell");
+                cell.textContent =  num;
+                cell.id = "cell-" + i;
+                tabellina.appendChild(cell);
+        }
+
+        }
+
+        });
+
         btn.addEventListener("click", () => {
             let numero;
             do {
@@ -26,6 +56,11 @@
             const cell = document.getElementById("cell-" + numero);
             cell.classList.add("estr");
         });
+
+
+        
+
+
         
         
             
